3eb4685533ce5ce42e7d7fa1b44db59930749afb
Amit <amit.kumar45@sap.com>
2021-11-25 21:31:32 +0530

    Learning day 2 | 2nd commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,28 @@
 import "package:flutter/material.dart";
+import 'package:learning_flutter/pages/home_page.dart';
+import 'package:learning_flutter/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  // const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    int days = 20;
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("This is a welcome app"),
-          ),
-        ),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark
       ),
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home":(context) => HomePage(),
+        "/login":(context) => LoginPage()
+      },
     );
   }
 }
